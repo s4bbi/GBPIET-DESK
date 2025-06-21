4ffba3e72321ce68eaddf77cac548b1ff87ef8ac
Yashpreet Singh <singhyashpreet22@gmail.com>
2025-06-21 21:59:13 +0530

    pull commit

--- backend/src/routes/v1/admin.routes.js
+++ backend/src/routes/v1/admin.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router();`
 
 const { AdminController } = require("../../controllers");
 const { AuthMiddleware, UserMiddleware } = require("../../middlewares");
