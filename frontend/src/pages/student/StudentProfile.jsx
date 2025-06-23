b2bea19e26c489d6b87307608cde6725ca2d208f
AyushJoshi123 <joshiayush00712345@gmail.com>
2025-06-24 01:57:04 +0530

    changes pull

--- frontend/src/pages/student/StudentProfile.jsx
+++ frontend/src/pages/student/StudentProfile.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import StudentLayout from '../../components/layouts/StudentLayout';
 import { HiOutlineAcademicCap } from "react-icons/hi";
 import axios from "axios";
